935182d533cd536a3e40568e68e18e1f872cc84f
Emily <emilysandberg04@icloud.com>
2026-05-09 13:12:44 +0200

    Fixade plottning och la till beräkning av V

--- MiniProject2.py
+++ MiniProject2.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 TD = np.load('TrainDigits.npy')
 TL = np.load('TrainLabels.npy')
 
+# Sorting the digits
 TL_flat = TL.flatten()
 
 A = []
@@ -13,9 +14,10 @@ for  i in range(10):
     A.append(digits_i[:, : 400])
 
 
-
+# Computing the SVD
 Sigma = []
 U = []
+VT = []
 
 for i in range(10):
     Ai = A[i]
@@ -23,7 +25,11 @@ for i in range(10):
     # Singular values
     eigval_singval_i = np.abs(np.linalg.eigvals(Ai.T @ Ai))
     singval_i = np.sqrt(eigval_singval_i)
-    Sigma.append(singval_i)
+
+    idx = np.argsort(singval_i)[::-1]
+    singval_i = singval_i[idx]
+    
+    Sigma.append(np.diag(singval_i))
 
     # U
     eigval_U_i, eigvec_U_i = np.linalg.eigh(Ai @ Ai.T)
@@ -32,32 +38,43 @@ for i in range(10):
     eigvec_U_i = eigvec_U_i[:, idx]
     U.append(eigvec_U_i)
 
+    # V^T
+    eigval_V_i, eigvec_V_i = np.linalg.eigh(Ai.T @ Ai)
+    idx = np.argsort(eigval_V_i)[::-1]
+
+    eigvec_V_i = eigvec_V_i[:, idx]
+    VT.append(eigvec_V_i.T)
 
 
+# Plotting singular values
 digits = [Sigma[3], Sigma[8]]
-fig, axes = plt.subplots(2, 1)
 
 for r in range(2):
-    axes[r].plot(digits[r])
+    sv = np.diag(digits[r])  # extract the diagonal
+    fig, ax = plt.subplots()
+    ax.plot(sv)
+    if r==0:
+        ax.set_title(f'Singular values for digit 3')
+    elif r==1:
+        ax.set_title(f'Singular values for digit 8')
 
-plt.tight_layout()
-plt.show()
+    plt.show()
 
-
+# Plottign singular images
 digits = [U[3], U[8]]
-
-fig, axes = plt.subplots(2, 3)
 
 for r in range(2):
     for c in range(3):
+        fig, ax = plt.subplots()
         img = np.reshape(digits[r][:, c], (28, 28)).T
-        #axes[r, c].imshow(img, cmap='gray')
-        #axes[r, c].imshow(img, cmap='seismic'r, c)
-        axes[r, c].imshow(np.abs(img), cmap='gray')
-        axes[r, c].axis('off')
+        ax.imshow(np.abs(img), cmap='gray')
+        ax.axis('off')
+        if r==0:
+            ax.set_title(f'Singular image nr. {c+1} for digit 3')
+        elif r==1:
+            ax.set_title(f'Singular image nr. {c+1} for digit 8')
 
-plt.tight_layout()
-plt.show()
+        plt.show()
 
 
 #sum = np.sum(index)
